cd2a78ad83e0732a6feddb7e80b8b678f652a890
FHMohara <fhmohara@outlook.com>
2019-03-10 18:32:46 -0300

    Docker Swarm

--- mysql/iknowit.sql
+++ mysql/iknowit.sql
@@ -1,3 +1,4 @@
 use iknowitdb;
+DROP TABLE IF EXISTS Ingredientes;
 create table if not exists Ingredientes (id INT(6) PRIMARY KEY, name VARCHAR(30) NOT NULL);
 insert into Ingredientes values (1, "Gordura polisaturada");
